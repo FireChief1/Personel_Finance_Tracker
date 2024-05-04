cbc0e44898b32342fc8b694a3f48acee7e2cb6df
ErkutAtes <erkutates54@gmail.com>
2024-05-04 17:36:02 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Personel_Finance_Tracker.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
